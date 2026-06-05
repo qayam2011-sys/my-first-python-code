4ef02602c20353b3292feb056da0bedf30af77d8
qayam2011-sys <qayam2011@gmail.com>
2026-06-05 23:31:14 +0530

    added a simple intrest calculator functions

---
+++ sicalc.py
@@ -0,0 +1,8 @@
+#p*t*r/100
+def sicalc():
+    p=float(input("Enter the principal amount: "))
+    t=float(input("Enter the time in years: "))
+    r=float(input("Enter the rate of interest: "))
+    si=(p*t*r)/100
+    print(f"The simple interest is: {si}")   
+
